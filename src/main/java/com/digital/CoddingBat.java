e1a53200a9b64efe7c44b9cd5a2d7bc74838f546
Ulan <ulan.torobekov@gmail.com>
2023-04-06 09:36:35 +0600

    first-commit

---
+++ src/main/java/com/digital/CoddingBat.java
@@ -0,0 +1,13 @@
+package com.digital;
+
+public class CoddingBat {
+
+
+    public String stringTimes(String str, int n) {
+        String result = "";
+        for (int i=0; i<n; i++) {
+            result = result + str;  // could use += here
+        }
+        return result;
+    }
+}
